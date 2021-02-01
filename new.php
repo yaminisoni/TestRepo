72cfd7aac4427c3974cf798375db55b5979cb8df
yaminisoni <30685911+yaminisoni@users.noreply.github.com>
2021-02-01 20:42:54 +0530

    Create new.php

---
+++ new.php
@@ -0,0 +1,22 @@
+
+<div class="section section-about-cs pt-70 pb-70">
+   <div class="container">
+      <div class="row-vertical row-vertical-align-center">
+         <div class="col-md-12">
+            <h2 class="title titlewhite" data-aos="zoom-in">
+               About <span class="span-white">EzappSolution</span>
+            </h2>
+            <p class="paragraph paragraph-white">
+               EzappSolution is onshore and offshore product development partner for mission-driven technology
+               startups across the globe. We combine business expertise and cutting-edge technology to drive
+               success for our customers and help them win in their chosen markets.
+            </p>
+            <a data-w-id="04e68a39-e574-3c4e-5bcd-262709a58f82" href="<?php echo base_url(); ?>contact" class="btn w-inline-block" data-aos="zoom-in">
+               <div class="btn-line-before"></div>
+               <div class="btn-text">Talk to us</div>
+               <div class="btn-line-after"></div>
+            </a>
+         </div>
+      </div>
+   </div>
+</div>
